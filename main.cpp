6dc6c612d374c5501b01b94920cfb12742b8ad61
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2026-09-14 14:57:38 +0530

    print events

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include "mainwindow.h"
+#include "linuxdraginput.h"
 
 #include <QApplication>
+#include <QThread>
+#include <cstdio>
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_QPA_PLATFORM", "xcb");
     QApplication a(argc, argv);
+    LinuxDragInput drag;
+    exit(0);
+    // QThread::create([] {
+    //     LinuxDragInput drag;
+    //     drag.onDrag([](int dx, int dy) {
+    //         printf("drag delta: %d, %d\n", dx, dy);
+    //         fflush(stdout);
+    //     });
+    //     drag.run();
+    // })->start();
+
     MainWindow w;
     w.show();
     return a.exec();
