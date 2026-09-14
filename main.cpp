af5f706bc1d87f99eae9647713c83cee08eaf2fd
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2026-09-14 17:22:11 +0530

    add macos shake detection

--- main.cpp
+++ main.cpp
@@ -6,14 +6,27 @@
 #include "linuxdragshakedetector.h"
 #endif
 
+#ifdef Q_OS_MAC
+#include "macosdragshakedetector.h"
+#endif
+
 int main(int argc, char *argv[])
 {
+    #ifdef Q_OS_LINUX
     qputenv("QT_QPA_PLATFORM", "xcb");
+#endif
     QApplication a(argc, argv);
 
 #ifdef Q_OS_LINUX
     LinuxDragShakeDetector detector;
     QObject::connect(&detector, &LinuxDragShakeDetector::shakeDetected, &a, [] {
+        qInfo() << "Shake detected";
+    });
+#endif
+
+#ifdef Q_OS_MAC
+    MacOSDragShakeDetector detector;
+    QObject::connect(&detector, &MacOSDragShakeDetector::shakeDetected, &a, [] {
         qInfo() << "Shake detected";
     });
 #endif
