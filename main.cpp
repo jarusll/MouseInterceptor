34346c58b97a8bd390884c0a4aacff4453e35e40
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2026-09-15 22:09:31 +0530

    save

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
 
 int main(int argc, char *argv[])
 {
-    #ifdef Q_OS_LINUX
+#ifdef Q_OS_LINUX
     qputenv("QT_QPA_PLATFORM", "xcb");
 #endif
     QApplication a(argc, argv);
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QDir>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    model = new QFileSystemModel(this);
+    model->setRootPath(QDir::homePath() + "/Downloads");
+    ui->listView->setViewMode(QListView::IconMode);
+    ui->listView->setUniformItemSizes(true);
+    ui->listView->setGridSize(QSize(120, 100));
+    ui->listView->setMovement(QListView::Static);
+    ui->listView->setResizeMode(QListView::Adjust);
+    ui->listView->setWrapping(true);
+    ui->listView->setLayoutMode(QListView::Batched);
+    ui->listView->setModel(model);
+    ui->listView->setRootIndex(model->index(QDir::homePath() + "/Downloads"));
 }
 
 MainWindow::~MainWindow()
